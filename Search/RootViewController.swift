249d95fa2924328a4615d04ee45b27d7b0c6ba4a
Daniel Garbień <me@danielgarbien.com>
2017-02-05 09:30:45 +0100

    Replace storyboard with root vc.

---
+++ Search/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  Search
+//
+//  Created by Daniel Garbień on 2/5/17.
+//  Copyright © 2017 Daniel Garbień. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+}
