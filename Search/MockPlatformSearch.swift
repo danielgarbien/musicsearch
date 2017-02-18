1889fee5134a2fa170d31574392b10c8b9979a2b
Daniel Garbień <me@danielgarbien.com>
2017-02-18 12:53:55 +0100

    Improve mock results.

--- Search/MockPlatformSearch.swift
+++ Search/MockPlatformSearch.swift
@@ -23,7 +23,7 @@ extension MockPlatformSearch: PlatformSearch {
     
     func load(entityOfType: Entity, didChange: @escaping (PlatformSearch) -> Void) {
         OperationQueue.main.addOperation {
-            self.state[entityOfType] = .loaded(results: ["witka", "siema", "elo"])
+            self.state[entityOfType] = .loaded(results: entityOfType.mockResults())
             didChange(self)
         }
     }
@@ -33,4 +33,13 @@ extension MockPlatformSearch: PlatformSearch {
     }
 }
 
-
+fileprivate extension Entity {
+    
+    func mockResults() -> [String] {
+        switch self {
+        case .artist: return ["Run the Jewels", "James Blake"]
+        case .album: return ["OK Computer", "The Colour of Spring"]
+        case .song: return ["Paralyzed", "I wanna be adored", "Song 2"]
+        }
+    }
+}
